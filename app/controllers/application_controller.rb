3c15b44272d644b5c395d160f20f07e5fd91af20
Leonardo Faria <leonardofaria@gmail.com>
2015-08-23 15:53:26 -0300

    Allow authentication via username and email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :set_locale, :set_style
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def set_style
     style = params[:style] ? params[:style] : session[:style] ? session[:style] : 'Monokai'
@@ -20,5 +21,13 @@ class ApplicationController < ActionController::Base
     end
 
     I18n.locale = locale
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,21 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :encryptable, encryptor: :restful_authentication_sha1
+         # :encryptable, encryptor: :restful_authentication_sha1,
+         :authentication_keys => [:login]
+
+	# Virtual attribute for authenticating by either username or email
+	# This is in addition to a real persisted field like 'username'
+	attr_accessor :login
+
+	validates :username, :presence => true, :uniqueness => { :case_sensitive => false }
+
+	def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions.to_h).first
+    end
+  end
 end
